0045a90de46f3f189b20900843df808ac9ad984a
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:41:13 +0530

    Create ResourcesData.sql

---
+++ ResourcesData.sql
@@ -0,0 +1,4 @@
+INSERT INTO Resources (event_id, resource_type, resource_url, uploaded_at) VALUES
+(2, 'pdf', 'https://portal.com/resources/tech_meetup_agenda.pdf', '2025-05-01 10:00:00'),
+(2, 'image', 'https://portal.com/resources/ai_poster.jpg', '2025-04-20 09:00:00'),
+(3, 'link', 'https://portal.com/resources/html5_docs', '2025-06-25 15:00:00');
