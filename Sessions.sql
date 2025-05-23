51c0efe903d40860e3abcf86bdb0afef89a7d4e5
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:17:09 +0530

    Create Sessions.sql

---
+++ Sessions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Sessions (
+    session_id INT PRIMARY KEY AUTO_INCREMENT,
+    event_id INT,
+    title VARCHAR(200) NOT NULL,
+    speaker_name VARCHAR(100) NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
