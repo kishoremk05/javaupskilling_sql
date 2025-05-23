5093b6226aa5ff4907f72f1081a4fc6f86b69936
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:20:07 +0530

    Create Feedback.sql

---
+++ Feedback.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Feedback (
+    feedback_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    event_id INT,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
+    feedback_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
