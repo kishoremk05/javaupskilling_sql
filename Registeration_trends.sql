3f17094036e65a0ca61620f4d4cb69eb5f89157b
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 03:04:07 -0700

    Delete Registeration_trends.sql

--- Registeration_trends.sql
+++
@@ -1,5 +0,0 @@
-SELECT DATE_FORMAT(registration_date, '%Y-%m') AS month, COUNT(*) AS registration_count
-FROM Registrations
-WHERE registration_date >= CURRENT_DATE - INTERVAL 12 MONTH
-GROUP BY month
-ORDER BY month;
