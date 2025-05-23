e032c4d57085b10bac1204d3f137721c3411bd9c
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:28:45 +0530

    Update Resources.sql

--- Resources.sql
+++ Resources.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE Resources (
+    resource_id INT PRIMARY KEY AUTO_INCREMENT,
+    event_id INT,
+    resource_type ENUM('pdf', 'image', 'link'),
+    resource_url VARCHAR(255) NOT NULL,
+    uploaded_at DATETIME NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
