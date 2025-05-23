59efddc71adec650819b1946428fe27c973fd13b
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:19:02 +0530

    Create Registration.sql

---
+++ Registration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Registrations (
+    registration_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    event_id INT,
+    registration_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
