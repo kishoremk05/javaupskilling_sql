54bb8723bad94a967a513bd571ab287d653f0590
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:14:11 +0530

    Create Users.sql

---
+++ Users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    registration_date DATE NOT NULL
+);
