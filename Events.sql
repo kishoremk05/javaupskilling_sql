1edf5919e903587343e3f7d4957de6839be2733b
kishoremk05 <kishore.05mk@gmail.com>
2025-05-23 10:15:41 +0530

    Create Events.sql

---
+++ Events.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Events (
+    event_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    city VARCHAR(100) NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    status ENUM('upcoming', 'completed', 'cancelled'),
+    organizer_id INT,
+    FOREIGN KEY (organizer_id) REFERENCES Users(user_id)
+);
